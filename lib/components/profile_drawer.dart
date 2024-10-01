55c4e710fff4e8c2fec015fdabe95b6d8f6dd0bf
Hojeong <queen00220@naver.com>
2024-10-01 18:06:36 +0900

    drawer 작업중

--- lib/components/profile_drawer.dart
+++ lib/components/profile_drawer.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+
 class ProfileDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 200,
       height: double.infinity,
-      color: Colors.blue,
+      color: Colors.yellow,
+      child: ListView(
+        children: [
+          for (int i = 0; i < 100; i++) Text("하하하하하하하하하하하하하하하"),
+        ],
+      ),
     );
   }
 }
